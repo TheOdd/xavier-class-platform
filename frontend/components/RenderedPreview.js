9c44bef16c54945554ff50918d5ea0b6f56608a2
TheOdd <owenhines07@gmail.com>
2018-02-07 11:52:56 -0600

    Hijacked console.log for editor

--- frontend/components/RenderedPreview.js
+++ frontend/components/RenderedPreview.js
@@ -1,33 +1,63 @@
 import React, { Component } from 'react'
 import renderHTML from 'react-render-html'
+import uuidv1 from 'uuid/v1'
 
 const htmlTemplate = `
     <h1>Test</h1>
 `
 
+const oldLog = console.log
+
 export default class RenderedPreview extends Component {
     constructor(props) {
         super(props)
+        const newState = []
         try {
+            console.log = function (msg) {
+                newState.push(msg)
+            }.bind(this)
             eval(props.code)
         } catch (err) {
-            
+            console.log = function (msg) {
+                newState.push(msg)
+            }.bind(this)
+            console.log(err)
+        } finally {
+            this.state = {
+                logMessages: newState
+            }
+            console.log = oldLog
         }
     }
 
     componentWillReceiveProps(nextProps) {
+        const newState = []
         try {
+            console.log = function (msg) {
+                newState.push(msg)
+            }.bind(this)
             eval(nextProps.code)
-            this.forceUpdate()
         } catch (err) {
-
+            console.log = function (msg) {
+                newState.push(msg)
+            }.bind(this)
+            console.log(err)
+        } finally {
+            this.setState({
+                logMessages: newState
+            })
+            console.log = oldLog
         }
     }
 
     render() {
         return (
             <div id="preview">
-                {renderHTML(htmlTemplate)}
+                <ul>
+                    {this.state.logMessages.map(msg => (
+                        <li key={uuidv1()}>{msg}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
--- frontend/containers/AppContainer.js
+++ frontend/containers/AppContainer.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import Main from '../components/Main'
 import RenderedPreview from '../components/RenderedPreview'
 import Editor from '../components/Editor'
 
 const AppContainer = ({ flipSwitch, code, dispatch }) => (
     <div>
-        <Main />
         <Editor dispatch={dispatch} />
         <RenderedPreview code={code} flipSwitch={flipSwitch} />
     </div>
